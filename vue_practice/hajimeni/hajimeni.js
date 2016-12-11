d5466a4f02eae948be9776c2570d33b9d7c95b4a
yuyutyanu <yuyutyanu@yahoo.co.jp>
2016-12-12 01:45:36 +0900

    完了

--- vue_practice/hajimeni/hajimeni.js
+++ vue_practice/hajimeni/hajimeni.js
@@ -1,3 +1,10 @@
+Vue.component('todo-item', {
+  // todo-item コンポーネントはカスタム属性のような "プロパティ" で受け取ります。
+  // このプロパティは todo と呼ばれます。
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
 new Vue({
   el:'#app',
   data:{
@@ -9,7 +16,12 @@ new Vue({
       {text:'hogehoge'},
       {text:'hogehoge'},
       {text:'hogehoge'},
-    ]
+    ],
+    groceryList: [
+    { text: 'Vegetables' },
+    { text: 'Cheese' },
+    { text: 'Whatever else humans are supposed to eat' }
+  ]
     },
     methods:{
       reverseMessage: function () {
